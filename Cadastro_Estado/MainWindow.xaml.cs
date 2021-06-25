bdd13697220862def2bf1be664fa3100ee015a1a
Milena Evangelista Vidal <milenajipa2015@gmail.com>
2021-06-25 15:40:53 -0400

    Add código na tela Estado, add classe estado

--- Cadastro_Estado/MainWindow.xaml.cs
+++ Cadastro_Estado/MainWindow.xaml.cs
@@ -20,9 +20,50 @@ namespace Cadastro_Estado
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Estado estado = new Estado();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+ 
+        private void ClearTextBox()
+        {
+            txtNomeestado.Text = "";
+            txtPaisestado.Text = "";
+            txtSiglaestado.Text = "";
+        }
+
+        private void BtnSalvarEst_Click(object sender, RoutedEventArgs e)
+        {
+
+            estado.Nomeestado = txtNomeestado.Text;
+            estado.Paisestado = txtPaisestado.Text;
+            estado.Siglaestado = txtSiglaestado.Text;
+
+            if (estado.Nomeestado.Equals(""))
+            {
+                MessageBox.Show("O campo Nome do Estado está vazio!!!");
+            }
+
+            if (estado.Paisestado.Equals(""))
+            {
+                MessageBox.Show("O campo País em que o Estado se localiza está vazio!!!");
+            }
+            if (estado.Siglaestado.Equals(""))
+            {
+                MessageBox.Show("O campo Sigla do Estado está vazio!!!");
+            }
+
+            else
+            {
+                MessageBox.Show($"Nome do Estado:{ estado.Nomeestado}\n" +
+                    $"País em que o Estado se localiza:{ estado.Paisestado}\n" +
+                    $"Sigla do Estado:{ estado.Siglaestado}");
+
+                ClearTextBox();
+            }
+
         }
     }
 }
