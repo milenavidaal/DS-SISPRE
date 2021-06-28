bb6d8943eb450745f1963acf6fa8909ba1ed5426
Ana Beatriz Grego <anabiagrego@gmail.com>
2021-06-28 15:08:45 -0300

    Tela Vender Produtos - Finalizado

---
+++ VenderProdutos/Venda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VenderProdutos
+{
+    public class Venda
+    {
+        public int Item { get; set; }
+        public String NomeProduto { get; set; }
+        public int Und { get; set; }
+        public float ValorUnitario { get; set; }
+        public int Quantidade { get; set; }
+        public int Desconto { get; set; }
+        public float ValorTotal { get; set; }
+        
+    }
+}
