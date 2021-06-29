e7ccf2badc9cecd1ea7a55823db2d2826c808edd
Vitória Maria de Jesus Miranda <vmariadejesusmiranda@gmail.com>
2021-06-29 19:05:49 -0400

    Cadastrar_CLIENTE

---
+++ Cadastrar_Cliente/Cadastrar_Cliente/adicionado.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cadastrar_Cliente
+{
+    /// <summary>
+    /// Lógica interna para adicionado.xaml
+    /// </summary>
+    public partial class adicionado : Window
+    {
+        public adicionado()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            MainWindow janela = new MainWindow();
+            janela.Show();
+        }
+    }
+}
---
+++ Cadastrar_Cliente/Cadastrar_Cliente/excluir.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cadastrar_Cliente
+{
+    /// <summary>
+    /// Lógica interna para excluir.xaml
+    /// </summary>
+    public partial class excluir : Window
+    {
+        public excluir()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            MainWindow janela = new MainWindow();
+            janela.Show();
+        }
+    }
+}
